95bdccde61156aa8c902e36fc026c067c2160c99
Joe Amenta <airbreather@linux.com>
2026-08-02 14:43:06 -0400

    Transport template
    I thought I got everything previously, but this was still unsaved in my IDE

--- typescript/classes/Transport.d.ts
+++ typescript/classes/Transport.d.ts
@@ -3,6 +3,10 @@ declare global {
 		class Transport extends GameObject {
 			readonly isMotionTransport: boolean;
 			readonly passengers: WorldObject[];
+
+			addPassenger(passenger: WorldObject, withAll?: boolean): void;
+			removePassenger(passenger: WorldObject, withAll?: boolean): void;
+			enableMovement(enabled: boolean): void;
 		}
 	}
 }
