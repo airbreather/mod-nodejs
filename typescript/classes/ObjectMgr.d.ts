ce31c86c324ce3852c0e7bc334f3eaa1b9a71755
Joe Amenta <airbreather@linux.com>
2026-08-19 06:47:54 -0400

    0.0.13:
    - quick tweak: forgot the 'persist' arg

--- typescript/classes/ObjectMgr.d.ts
+++ typescript/classes/ObjectMgr.d.ts
@@ -4,7 +4,7 @@ declare global {
 			private constructor();
 
 			static getNpcVendorItemList(vendor: number): ReadonlyVendorItemData | undefined;
-			static setNpcVendorItemList(vendor: number, data: VendorItemData): void;
+			static setNpcVendorItemList(vendor: number, data: VendorItemData, persist?: boolean): void;
 			static addVendorItem(vendor: number, item: number, maxCount: number, incrTime: Temporal.Duration, extendedCost: number, persist?: boolean): void;
 			static removeVendorItem(vendor: number, item: number, persist?: boolean): void;
 		}
